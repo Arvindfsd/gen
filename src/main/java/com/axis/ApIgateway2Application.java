6e667a9a02259f242bb3178e0ae26459ed96734e
Arvind <arvindfsd@gmail.com>
2024-01-30 23:28:20 +0530

    initial commit

---
+++ src/main/java/com/axis/ApIgateway2Application.java
@@ -0,0 +1,13 @@
+package com.axis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIgateway2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIgateway2Application.class, args);
+	}
+
+}
